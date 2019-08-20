f5c779772b8f2174be31021097fcb262d14e030b
JJ12880 <Justinj100@gmail.com>
2019-08-19 20:18:04 -0400

    Adds configurable API servers
    Loads api server list from config file, then uses first server to reply for all subsequent requests

---
+++ ConfigFileReadercs.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SmartChain.Web
+
+{
+    public class ConfigFileReader
+    {
+        private JObject options = new JObject();
+
+        public ConfigFileReader()
+        {
+            string path = Directory.GetCurrentDirectory().ToString() + "/config.conf";
+
+            if (!File.Exists(path)){
+                path = Directory.GetCurrentDirectory().ToString() + "/default.conf";
+            }
+             if (!File.Exists(path))
+                return;    
+
+                
+            using (StreamReader sr = new StreamReader(path))
+            {
+
+                string json = sr.ReadToEnd();
+               
+                try {
+                    options = JObject.Parse(json);
+
+                }
+                catch(Exception e)
+                {
+
+                }
+
+              
+            }
+        }
+
+
+      
+
+
+
+        public string lookup(string key)
+        {
+            if (!options.ContainsKey(key))
+                throw new Exception("The key '" + key + "' was not set in the config: " + Directory.GetCurrentDirectory().ToString());
+            return options[key].ToString();
+        }
+
+        public string lookup_or_default(string key)
+        {
+            if (!options.ContainsKey(key))
+                return "";
+            return options[key].ToString();
+        }
+
+        public bool haskey(string key)
+        {
+            return options.ContainsKey(key);
+        }
+    }
+}
