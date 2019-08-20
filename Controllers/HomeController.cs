b913284ecf831ab2a22e3b313dd1ffbae88f954e
JJ12880 <Justinj100@gmail.com>
2019-08-19 20:18:04 -0400

    Adds configurable API servers
    Loads api server list from config file, then uses first server to reply for all subsequent requests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 
 namespace SmartChain.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public static ConfigFileReader config = new ConfigFileReader();
         public IActionResult Index()
         {
             return View();
@@ -14,6 +16,12 @@ namespace SmartChain.Web.Controllers
         {
             ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
             return View();
+        }
+        [HttpGet]
+        [Route("api_servers")]
+        public JArray api_servers(string id)
+        {
+            return JArray.Parse(config.lookup("api_servers"));
         }
     }
 }
