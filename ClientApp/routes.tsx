8071f74c2f2f1231543aa98137a0edc7d05bb913
Justin <JustinJ100@gmail.com>
2021-05-21 09:30:06 -0400

    Remove old components

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -6,9 +6,6 @@ import { Records } from './components/MainPages/Records/Records'
 import { Elections } from './components/MainPages/Elections/Elections'
 import { Assets } from './components/MainPages/NFT/NFTs'
 import { AssetClasses } from './components/MainPages/NFTClasses/NFTClasses'
-import { Network } from './components/MainPages/NetworkStats/NetworkStats';
-import { Login } from './components/MainPages/Login/Login'
-import { MyProfile } from './components/MainPages/MyProfile/MyProfile'
 import { Messages } from './components/MainPages/SignedMessages/SignedMesages'
 import { Tools } from './components/MainPages/Tools/Tools'
 
@@ -19,9 +16,7 @@ export const routes = <Layout>
     <Route path='/elections' component={Elections} />
     <Route path='/NFTs' component={Assets} />
     <Route path='/NFTgroups' component={AssetClasses} />
-    <Route path='/stats' component={Network} />
-    <Route path='/login' component={Login} />
-    <Route path='/myprofile' component={MyProfile} />
+   
     <Route path='/logout' component={Users} />
     <Route path='/messages' component={Messages} />
     <Route path='/tools' component={Tools} />
